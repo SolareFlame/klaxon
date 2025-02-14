17d64a9500937d804bdbb99bfc06f160ab7c00fc
SolareFlame <zeldac332@gmail.com>
2025-02-14 20:44:53 +0100

    🚧 DB Models + Factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
         'pass',
         'perm_level',
         'icetoken',
+        'is_listening',
     ];
 
     protected $hidden = [
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,44 +1,27 @@
 <?php
-
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
+            'id' => '@' . fake()->userName(),
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'desc' => fake()->sentence(),
+            'pp' => fake()->imageUrl(100, 100, 'people'),
+            'pass' => bcrypt('password'),
+            'perm_level' => fake()->numberBetween(0, 10),
+            'icetoken' => Str::random(32),
+            'is_listening' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
