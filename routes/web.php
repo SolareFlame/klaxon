b9c1dc7c951fba5ef8a33ffa21cf7c1f97ebd380
SolareFlame <zeldac332@gmail.com>
2025-02-16 19:22:18 +0100

    🚧 Api Stream Connexion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/icecast/auth', function (Request $request) {
+    $token = $request->query('token');
+
+    $user = User::where('icetoken', $token)->first();
+
+
+    if (!$user || $user->is_listening) {
+        return response('Unauthorized', 403);
+    }
+
+    $user->is_listening = true;
+    $user->save();
+
+    return response('OK', 200);
+});
+
+Route::get('/icecast/disconnect', function (Request $request) {
+    $token = $request->query('token');
+
+    $user = User::where('icetoken', $token)->first();
+    if ($user) {
+        $user->is_listening = false;
+        $user->save();
+    }
+
+    return response('Disconnected', 200);
 });
